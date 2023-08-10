18617b0ab640333691c6339ce965ecfedff61736
HENRIQUE SANTOS ESPINDOLA <henrique.sespindola@senacsp.edu.br>
2023-08-10 12:00:57 -0300

    Feat: Conclusão do Header

--- App.js
+++ App.js
@@ -1,16 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
 import { styles } from './css/Styles';
+import { AntDesign, Ionicons, Octicons, MaterialCommunityIcons} from '@expo/vector-icons'; 
 
 export default function App() {
   return (
     <View style={styles.screen}>
+
       <View style={styles.header}>
         <View style={styles.headerTop}>
+          <View style={styles.headerIconUser}>
+            <AntDesign name="user" size={27} color="#fff" />
+          </View>
+
+          <View style={styles.headerTopRight}>
+            <Ionicons name="ios-eye-off-outline" size={25} color="#fff" />
+            <Octicons name="question" size={25} color="#fff" />
+            <MaterialCommunityIcons name="email-plus-outline" size={25} color="#fff" />
+          </View>
+        </View>
+
+        <Text style={styles.txtUser}>Olá, Henrique!</Text>
+      </View>
+
+      <View style={styles.account}>
+        <View style={styles.accountLine}>
+          <Text style={styles.txtAccount}>Conta</Text>
+          <Ionicons name="ios-chevron-forward" size={25} color="#000" />
+        </View>
+        
+        <View style={styles.accountInfo}>
+          <Text style={styles.txtMoney}>R$ 800,25</Text>
         </View>
       </View>
 
+      <View style={styles.nav}>
+        <View style={styles.navItem}>
+          <View style={styles.bgNav}>
+            <Image style={styles.imgPix} source={{uri:
+              ("https://img.icons8.com/ios/50/pix.png")}} />
+          </View>
+          <Text style={styles.txtNav}>Pix</Text>
+        </View>
+        
+        <View style={styles.navItem}>
+          <View style={styles.bgNav}>
+            <AntDesign style={styles.imgPix} name="barcode" size={25} color="black" />
+          </View>
+          <Text style={styles.txtNav}>Pagar</Text>
+        </View>
 
+        <View style={styles.navItem}>
+          <View style={styles.bgNav}>
+            <Image style={styles.imgPix} source={{uri:
+              ("https://img.icons8.com/ios/50/pix.png")}} />
+          </View>
+          <Text style={styles.txtNav}>Pix</Text>
+        </View>
+
+        <View style={styles.navItem}>
+          <View style={styles.bgNav}>
+            <Image style={styles.imgPix} source={{uri:
+              ("https://img.icons8.com/ios/50/pix.png")}} />
+          </View>
+          <Text style={styles.txtNav}>Pix</Text>
+        </View>
+        
+      </View>
+
+      <View style={styles.card}></View>
+
+      <View style={styles.info}></View>
+
+      <View style={styles.account}></View>
+
+      <View style={styles.footer}></View>
 
       <StatusBar style="auto" />
     </View>
--- css/Styles.js
+++ css/Styles.js
@@ -8,29 +8,103 @@ export const styles = StyleSheet.create({
         alignItems: "center",
     },  
     header:{
-        flex: 1,
+        flex: 1.4,
         backgroundColor: "#820ad1",
-        width: "100%"
+        width: "100%",
     },
     headerTop:{
-
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingRight: 35,
+        paddingLeft: 35,
+    },
+    headerTopRight:{
+        flexDirection: "row",
+        gap: 35,
+        paddingTop: 70,
+    },
+    headerIconUser:{
+        backgroundColor: "#9e29e7",
+        padding: 8,
+        marginTop: 60,
+        borderRadius: 50,
+    },
+    txtUser:{
+        color: "#fff",
+        fontSize: 17,
+        margin: 28,
     },
     account:{
-
+        flex: 0.9,
+        backgroundColor: "#f1f1f1",
+        width: "100%",
     },
+    accountLine:{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingLeft: 35,
+        paddingRight: 35,
+        marginTop: 25
+    },
+    txtAccount:{
+        fontSize: 17,
+        fontWeight: "bold",
+    },
+     
     accountInfo:{
-
+        paddingLeft: 35,
+        paddingTop: 20,
+    },
+    txtMoney:{
+        fontSize: 22,
+        fontWeight: "bold",
     },
     nav:{
-
+        flex: 1,
+        backgroundColor: "#f1f1f1",
+        width: "100%",
+        flexDirection: "row",
+    },
+    navItem:{
+        justifyContent: "center",
+        alignItems: "center",
+        width: "33%",
+        marginTop: 25,
+    },
+    bgNav:{
+        width: 60,
+        height: 60,
+        borderRadius: 100,
+        backgroundColor: "#ddd",
+    },
+    imgPix:{
+        height: 25,
+        width: 25,
+        position: "relative",
+        marginLeft: 17,
+        marginTop: 17,
+    }, 
+    txtNav:{
+        paddingRight:2,
+        marginTop: 10,
     },
     card:{
-
+        flex: 1,
+        backgroundColor: "#f1f1f1",
     },
     info:{
-
+        flex: 1,
+        backgroundColor: "#f1f1f1",
     },
     footer:{
-
+        
     },
+
+    // Pré-definição de margem para todos os lados
+    margin20:{
+        margin: 20,
+    },
+    margin30:{
+        margin: 30,
+    }
 });
